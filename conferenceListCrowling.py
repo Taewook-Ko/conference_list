472b8d0a15c47877b3f5f3a27b65838e9943cb25
Taewook <paramount@kaist.ac.kr>
2019-01-22 11:19:29 +0900

    first commit

--- conferenceListCrowling.py
+++ conferenceListCrowling.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
 
-browser = webdriver.Chrome('PATH') # path where chromedriver_win32\\chromedriver exist
+#PATH = 'C:\\Users\\JihyunP\\Downloads\\chromedriver_win32\\chromedriver'
+browser = webdriver.Chrome(PATH) # path where chromedriver_win32\\chromedriver exist
 url = 'https://cse.snu.ac.kr/node/29041'
 browser.get(url)
 html_source = browser.page_source
@@ -15,4 +16,5 @@ for i in range(0,len(tds),2):
     lists.append(tds[i].text)
 
 df = pd.DataFrame(lists)
+#PATH = 'C:\\Users\\JihyunP\\Desktop\\test.csv'
 df.to_csv(PATH+'\\test.csv')  # path where to store csv file
