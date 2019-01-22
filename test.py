04def348bf28358330a3d2747f17b1177889ca93
Taewook <paramount@kaist.ac.kr>
2019-01-22 11:16:34 +0900

    first commit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
 
-browser = webdriver.Chrome('C:\\Users\\JihyunP\\Downloads\\chromedriver_win32\\chromedriver')
+browser = webdriver.Chrome('PATH') # path where chromedriver_win32\\chromedriver exist
 url = 'https://cse.snu.ac.kr/node/29041'
 browser.get(url)
 html_source = browser.page_source
@@ -15,4 +15,4 @@ for i in range(0,len(tds),2):
     lists.append(tds[i].text)
 
 df = pd.DataFrame(lists)
-df.to_csv('C:\\Users\\JihyunP\\Desktop\\test.csv')
+df.to_csv(PATH+'\\test.csv')  # path where to store csv file
