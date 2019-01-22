7f584a632cb0a69d2d0ace37842ebf5120f2ff66
Taewook <paramount@kaist.ac.kr>
2019-01-22 17:13:52 +0900

    first commit

--- RecSys18Accepted.py
+++ RecSys18Accepted.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 PATH = 'C:\\Users\\JihyunP\\Downloads\\chromedriver_win32\\chromedriver'
 browser = webdriver.Chrome(PATH) # path where chromedriver_win32\\chromedriver exist
-url ='https://recsys.acm.org/recsys18/accepted-contributions/'
+url ='https://recsys.acm.org/recsys18/accepted-contributions/'  #recsys2018 accepted papers
 browser.get(url)
 html_source = browser.page_source
 browser.quit()
